e5811a662d755c0a450db04fe6647b27e5666338
lucifermorningstar1305 <adityam.ghosh@gmail.com>
2024-01-03 02:49:33 +1300

    Fixed: Keyboard Mouse logging

--- keyboard_mouse_events.py
+++ keyboard_mouse_events.py
@@ -1,80 +1,57 @@
-"""
-Citation: https://discuss.python.org/t/running-keyboard-and-mouse-listener-at-the-same-time/13416/4
-
-"""
-
+from typing import Tuple, List, Dict, Any 
 from pynput import keyboard, mouse
+
 from collections import defaultdict
-import time
-import math
-import gc
+
+def on_move(x, y) -> Tuple:
+    ACTIONS["mouse"].append((x, y))
 
 
-def on_move(x, y):
-    pass
-
-
-def on_press(key):
-    print(f"Key pressed: {key}")
+def on_press(key) -> str:
+   ACTIONS["keyboard"].append(key)
 
 
 def on_release(key):
-    pass
+
+    if key == keyboard.Key.esc:
+        mlistener.stop()
+        klistener.stop()
+        return False
+    
 
 
-def onclick(x, y, button, pressed):
+def on_click(x, y, button, pressed) -> str:
     if pressed:
-        print(button)
+       if button == mouse.Button.left:
+           ACTIONS["mouse"].append("left")
+       
+       elif button == mouse.Button.right:
+          ACTIONS["mouse"].append("right")
+       
+       elif button == mouse.Button.middle:
+           ACTIONS["mouse"].append("middle")
+       
+       else:
+           return ""
 
 
-def onscroll(x, y, dx, dy):
+def on_scroll(x, y, dx, dy) -> str:
     try:
-        print(dy)
+        if dy > 0:
+           ACTIONS["mouse"].append("scroll_up")
+        else:
+            ACTIONS["mouse"].append("scroll_down")
     except Exception as exc:
         pass
 
-
-def checkMouseEvent():
-    with mouse.Events() as mevents:
-        mevent = mevents.get(0.1)
-
-        if mevent is not None:
-            if "Move" in str(mevent):
-                return on_move(mevent.x, mevent.y)
-
-            if "Click" in str(mevent):
-                return onclick(mevent.x, mevent.y, mevent.button, mevent.pressed)
-
-            if "Scroll" in str(mevent):
-                return onscroll(mevent.x, mevent.y, mevent.dx, mevent.dy)
-
-
-def checkKeyboardEvent():
-    with keyboard.Events() as kevents:
-        kevent = kevents.get(0.1)
-
-        if kevent is not None:
-            if "Press" in str(kevent):
-                on_press(kevent.key)
-
-
 if __name__ == "__main__":
-    while True:
-        ret = checkMouseEvent()
-        checkKeyboardEvent()
-        if ret is not None:
-            deltaX = 990 - ret[0]
-            deltaY = 540 - ret[1]
+    ACTIONS = defaultdict(lambda: list())
 
-            print(math.degrees(math.atan2(deltaY, deltaX)))
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as klistener, mouse.Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as mlistener:
+        
+        klistener.join()
+        mlistener.join()
 
+        print(klistener, mlistener)
 
-# import pyautogui
-
-# if __name__ == "__main__":
-#     try:
-#         while True:
-#             x, y = pyautogui.position()
-#             print(x, y)
-#     except Exception as e:
-#         print(e)
+    print(ACTIONS)
