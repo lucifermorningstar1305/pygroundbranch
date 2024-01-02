97cf07f36f4180bae963e339b5802084ff52c7ea
lucifermorningstar1305 <adityam.ghosh@gmail.com>
2024-01-03 04:05:19 +1300

    Added: win32api based mouse event listener

---
+++ keyboard_mouse_win32api.py
@@ -0,0 +1,18 @@
+import win32api as wapi
+import time
+
+
+def get_mouse_pos():
+    return wapi.GetCursorPos()
+
+def get_mouse_click():
+    special_keys = {
+        0x01: "leftClick",
+        0x02: "rightClick"
+    }
+
+    for i in range(1, 256):
+        if wapi.GetAsyncKeyState(i):
+            if i in special_keys:
+                return special_keys[i]
+            
--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 
-from keyboard_mouse_events import main
-from pynput import keyboard
+# from keyboard_mouse_events import main
+# from pynput import keyboard
 
+from keyboard_mouse_win32api import get_mouse_pos, get_mouse_click
 if __name__ == "__main__":
     
     while True:
-        ret = main()
-        if ret == keyboard.Key.f10:
-            print("DONE!")
-            break
+
+        print(get_mouse_pos())
+        print(get_mouse_click())
+        # ret = main()
+        # if ret == keyboard.Key.f10:
+        #     print("DONE!")
+        #     break
